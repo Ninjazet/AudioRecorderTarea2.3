00ceb12a747132a64204938f0a46c6a50ab267f0
Ninjazet <castellanostejada2001@gmail.com>
2024-03-07 13:12:50 -0600

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AudioRecorderTarea2._3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
